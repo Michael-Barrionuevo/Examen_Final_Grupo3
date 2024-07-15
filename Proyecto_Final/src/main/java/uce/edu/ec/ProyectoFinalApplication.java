e6341c8f38258d64c0532a2654c665772b98fae8
Jordi Pila <jordifpa7@gmail.com>
2024-07-15 08:44:53 -0500

    Clase Principal

--- Proyecto_Final/src/main/java/uce/edu/ec/ProyectoFinalApplication.java
+++ Proyecto_Final/src/main/java/uce/edu/ec/ProyectoFinalApplication.java
@@ -2,12 +2,18 @@ package uce.edu.ec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uce.edu.ec.View.Principal;
 
 @SpringBootApplication
 public class ProyectoFinalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoFinalApplication.class, args);
+	//	SpringApplication.run(ProyectoFinalApplication.class, args);
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Principal().setVisible(true);
+			}
+		});
 	}
 
 }
